b4c2d168d416e98307feb6cbab96bbca92438c74
221672372 <13173@hakodate.kosen-ac.jp>
2017-12-15 10:18:21 +0900

    あ

--- game/people.java
+++ game/people.java
@@ -12,8 +12,43 @@ public class people extends Actor
      * Act - do whatever the people wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
-        // Add your action code here.
+    public void act() {
+        
+        if( Greenfoot.isKeyDown( "shift" ) ){
+            if( Greenfoot.isKeyDown( "right" ) ){
+                setRotation(0);
+                move(1);
+            }
+            else if( Greenfoot.isKeyDown( "down" ) ){
+                setRotation(90);
+                move(1);
+            }
+            else if( Greenfoot.isKeyDown( "left" ) ){
+                setRotation(180);
+                move(1);
+            }
+            else if( Greenfoot.isKeyDown( "up" ) ){
+                setRotation(270);
+                move(1);
+            }
+        }
+        else{
+            if( Greenfoot.isKeyDown( "right" ) ){
+                setRotation(0);
+                move(10);
+            }
+            else if( Greenfoot.isKeyDown( "down" ) ){
+                setRotation(90);
+                move(10);
+            }
+            else if( Greenfoot.isKeyDown( "left" ) ){
+                setRotation(180);
+                move(10);
+            }
+            else if( Greenfoot.isKeyDown( "up" ) ){
+                setRotation(270);
+                move(10);
+            }
+        }
     }    
 }
