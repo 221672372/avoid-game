397e73265836c599293be0718ac0562b9e72bcaa
ryot1n <13133@hakodate.kosen-ac.jp>
2018-02-21 09:24:33 +0900

    02/21

--- game/Myworld.java
+++ game/Myworld.java
@@ -8,25 +8,20 @@ import greenfoot.*;
  */
 public class Myworld extends World
 {
-<<<<<<< HEAD
-
+    
     private int rtime;/* 制限時間 */   
     private int i;/* i...for文用制御変数 */
     private int x = 550;/* ❤のx座標調整用変数 */
-=======
-    private int rtime;
->>>>>>> 7cb6a6fde7e8e39a51a029230c6ee4de4e8b20cb
-    
+
     public void act()
     {
+        
         rtime--;
         showText("Time = "+rtime,500,50);
-<<<<<<< HEAD
-        showText("Life = "+title.life,500,70);
-=======
-       
->>>>>>> 7cb6a6fde7e8e39a51a029230c6ee4de4e8b20cb
-        
+
+            
+        //showText("Life = "+title.life,500,70);
+
         if(rtime == -1)
         {
             World game = new clear();
@@ -77,21 +72,29 @@ public class Myworld extends World
         rtime = 1000;
 
         addObject( new people(), 300, 200 );
+
+        
+         String heart ="";
+         for(i=0;i<title.life;i++){
+                heart += "❤";
+            }  
+        showText(heart,500,80);
         
         /*for(i=0;i<3;i++){
-            showText("❤",x,60);
-            x -= 10;
+        showText("❤",x,60);
+        x -= 10;
         }
-        
+
         if(people.life == 2){
-            showText("",x,60);
+        showText("",x,60);
         }
         if(people.life == 1){
-            showText("",x-10,60);
+        showText("",x-10,60);
         }
         if(people.life == 0){
-            showText("",x,60);
+        showText("",x,60);
         }*/
-        
+
     }
+    
 }
--- game/people.java
+++ game/people.java
@@ -8,23 +8,17 @@ import greenfoot.*;
  */
 public class people extends Actor
 {
-    private int Life;
-
-    public people(){
-        Life = 5;
-    }
-
     /**
      * Act - do whatever the people wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
 
-   // public static int hit = 0;/* 被弾判定用変数 */
+    // public static int hit = 0;/* 被弾判定用変数 */
     private int invtime = 0;/* invtime...無敵時間 */
 
     public void act() {
         operation();
-        
+
         if(invtime > 0 ){
             --invtime;
             if( (invtime % 2 ) == 1 ) getImage().setTransparency(0);
@@ -33,120 +27,68 @@ public class people extends Actor
         else hantei();
     }
 
-<<<<<<< HEAD
     public void operation(){
-        
-=======
-        getWorld().showText( "Life = "+Life,100, 50 );
 
->>>>>>> 7cb6a6fde7e8e39a51a029230c6ee4de4e8b20cb
         if( Greenfoot.isKeyDown( "shift" ) ){
-            
+
             if( Greenfoot.isKeyDown( "right" ) ){
                 setRotation(0);
-                move(1);
+                move(2);
             }
             else if( Greenfoot.isKeyDown( "down" ) ){
                 setRotation(90);
-                move(1);
+                move(2);
             }
             else if( Greenfoot.isKeyDown( "left" ) ){
                 setRotation(180);
-                move(1);
+                move(2);
             }
             else if( Greenfoot.isKeyDown( "up" ) ){
                 setRotation(270);
-                move(1);
+                move(2);
             }
         }
         else{
             if( Greenfoot.isKeyDown( "right" ) ){
                 setRotation(0);
-                move(10);
+                move(8);
             }
             else if( Greenfoot.isKeyDown( "down" ) ){
                 setRotation(90);
-                move(10);
+                move(8);
             }
             else if( Greenfoot.isKeyDown( "left" ) ){
                 setRotation(180);
-                move(10);
+                move(8);
             }
             else if( Greenfoot.isKeyDown( "up" ) ){
                 setRotation(270);
-                move(10);
+                move(8);
             }
         }
     }
-        
+
     public void hantei(){
-            
+
         Actor actor = getOneObjectAtOffset( 0, 0, BALL_S.class );
-        if( actor != null ){
-<<<<<<< HEAD
+        Actor actor2 = getOneObjectAtOffset( 0, 0, BALL_M.class );
+        Actor actor3 = getOneObjectAtOffset( 0, 0, BALL_L.class );
+        if( actor != null || actor2 != null || actor3 != null){
             title.life--;
+            String heart ="";
+
+            for(int i=0;i<title.life;i++){
+                heart += "❤";
+            }           
+            getWorld().showText(heart,500,80);
+
             if(title.life == -1)
             {
                 World game = new gameover();
                 Greenfoot.setWorld(game);
             }
             invtime = 100;
-=======
-            Life--;
-            getWorld().showText( "Life = "+Life,100, 50 );
-            getWorld().removeObject( actor );
-            if(Life == 0){
-                World game = new gameover();
-                Greenfoot.setWorld(game);
-            }
-
->>>>>>> 7cb6a6fde7e8e39a51a029230c6ee4de4e8b20cb
         }  
 
-        Actor actor2 = getOneObjectAtOffset( 0, 0, BALL_M.class );
-        if( actor2 != null ){
-<<<<<<< HEAD
-            title.life--;
-            if(title.life == -1)
-            {
-                World game = new gameover();
-                Greenfoot.setWorld(game);
-            }
-            invtime = 100;
-=======
-            Life--;
-            getWorld().showText( "Life = "+Life,100, 50 );
-            getWorld().removeObject( actor2 );
-       
-            if(Life == 0){
-                World game = new gameover();
-                Greenfoot.setWorld(game);
-            }
-
->>>>>>> 7cb6a6fde7e8e39a51a029230c6ee4de4e8b20cb
-        }
-
-        Actor actor3 = getOneObjectAtOffset( 0, 0, BALL_L.class );
-        if( actor3 != null ){
-<<<<<<< HEAD
-            title.life--;
-            if(title.life == -1)
-            {
-                World game = new gameover();
-                Greenfoot.setWorld(game);
-            }
-            invtime = 100;
-=======
-            Life--;
-            getWorld().showText( "Life = "+Life,100, 50 );
-            getWorld().removeObject( actor3 );
-        
-            if(Life == 0){
-                World game = new gameover();
-                Greenfoot.setWorld(game);
-            }
-
->>>>>>> 7cb6a6fde7e8e39a51a029230c6ee4de4e8b20cb
-        }
     }
 }
